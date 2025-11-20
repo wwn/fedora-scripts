3f182a809a08f711d80c844505d9f2cfaf33d83a
wwn <wwn@mailbox.org>
2025-11-20 20:14:03 +0100

    init

---
+++ post_install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y qflipper inkscape gimp mc
+
+dnf remove transmission xfburn
+
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+dnf install code
+
